eb2daebb3ca4d1f4570e4687ed5daaac430426fb
Anicod <animeshraj735@gmail.com>
2022-05-16 02:26:47 +0530

    added repetition till player reache winning 100

--- src/Snakeladder/snakeandladder.java
+++ src/Snakeladder/snakeandladder.java
@@ -9,25 +9,32 @@ public class snakeandladder {
         int x = 0;
         System.out.println("welcome to snake and ladder program");
         System.out.println("starting position of the player is :" + PLAYER_A_POSITION);
-        Random dice = new Random();
-        int DiceDispaly = dice.nextInt(6) + 1;
-        System.out.println("numbers display on the dice is:" + DiceDispaly);
-        Random check = new Random();
-        int PlayerAstatus = check.nextInt(3);
-        switch (PlayerAstatus) {
-            case 0:
-                System.out.println("current  position remains same:" + x);
+        while (x < 100) {
+            Random dice = new Random();
+            int DiceDispaly = dice.nextInt(6) + 1;
+            System.out.println("numbers display on the dice is:" + DiceDispaly);
+            Random check = new Random();
+            int PlayerAstatus = check.nextInt(3);
+            switch (PlayerAstatus) {
+                case 0:
+                    System.out.println("current  position remains same:" + x);
 
-                break;
-            case 1:
-                x = x + DiceDispaly;
+                    break;
+                case 1:
+                    x = x + DiceDispaly;
 
-                System.out.println("current player position is:" + x);
-                break;
-            default:
-                x = x - DiceDispaly;
-                System.out.println("current position is:" + x);
-                break;
+                    System.out.println("current player position is:" + x);
+                    break;
+                default:
+                    x = x - DiceDispaly;
+                    System.out.println("current position is:" + x);
+                    break;
+            }
+            if (x < 0) {
+                x = 0;
+                System.out.println("The player start from:" + x);
+            }
+
         }
     }
 }
