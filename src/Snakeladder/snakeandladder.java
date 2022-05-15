508e4ff2eb1c8269bac5ebde6a0385053675c862
Anicod <animeshraj735@gmail.com>
2022-05-16 01:44:12 +0530

    added starting position of the player

--- src/Snakeladder/snakeandladder.java
+++ src/Snakeladder/snakeandladder.java
@@ -1,9 +1,11 @@
 package Snakeladder;
 
-
 public class snakeandladder {
+    final static int PLAYER_A_POSITION = 0;
+
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder program");
+        System.out.println("starting position of the player is :" + PLAYER_A_POSITION);
     }
 }
 
