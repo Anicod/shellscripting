38d6feaf8f72abc8800e3a1a83ca2e5d08be1a12
Anicod <animeshraj735@gmail.com>
2022-05-16 01:57:23 +0530

    added player rolls a die to get the number

--- src/Snakeladder/snakeandladder.java
+++ src/Snakeladder/snakeandladder.java
@@ -1,11 +1,16 @@
 package Snakeladder;
 
+import java.util.Random;t
+
 public class snakeandladder {
     final static int PLAYER_A_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("welcome to snake and ladder program");
         System.out.println("starting position of the player is :" + PLAYER_A_POSITION);
+        Random dice = new Random();
+        int DiceDispaly = dice.nextInt(6) + 1;
+        System.out.println("numbers display on the dice is:" + DiceDispaly);
     }
 }
 
